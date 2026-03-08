e9fc86ce1ef05279970eaaa9cd874e68baf03161
JopaSgorela <vasyamos67@gmail.com>
2026-03-08 19:51:23 +0200

    fix: return full array in makeArray if length <= maxLength

--- js/task-2.js
+++ js/task-2.js
@@ -3,6 +3,7 @@ function makeArray(firstArray, secondArray, maxLength) {
     if (newArray.length > maxLength) {
         return newArray.slice(0, maxLength);
     }
+    return newArray;
 }
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
